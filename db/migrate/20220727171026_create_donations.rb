65e1d7c7b2b40741d25430339e6118b68fb9ee3f
Tom Tobar <cocdetombomb@gmail.com>
2022-07-27 12:17:10 -0500

    add migrations

---
+++ db/migrate/20220727171026_create_donations.rb
@@ -0,0 +1,11 @@
+class CreateDonations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :donations do |t|
+      t.float :amount #float type
+      t.datetime :datetime #date type
+      t.boolean :completed, :default => false #boolean type with default value
+      t.belongs_to :organization # This will assign a foreign key with index. You could also define this like: t.integer :organization_id
+      t.belongs_to :donor 
+    end
+  end
+end
